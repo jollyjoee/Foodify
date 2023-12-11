63d8063e5a58f6d5c226a388192ce571fb304590
jollyjoee <pisk.julijo@gmail.com>
2023-12-11 21:20:09 +0800

    click outside menu closes it. fixed menu adspace

--- js.js
+++ js.js
@@ -1,16 +1,33 @@
 var show = false;
+const menu = document.querySelector(".profilemenu");
+const footer = document.querySelector(".footer");
 
 function showSideMenu() {
-    let menu = document.querySelector(".profilemenu");
     if (show == false) {
         show = true;
         console.log(show);
         menu.style.display = "block";
+        footer.style.borderTopRightRadius = "0px";
     }
     else {
         show = !show;
         console.log(show);
         menu.style.display = "none";
+        footer.style.borderTopRightRadius = "15px";
     }
 
 }
+
+document.querySelector('body').addEventListener('click', function(){
+    show = !show;
+    menu.style.display = "none";
+    footer.style.borderTopRightRadius = "15px";
+  });
+  
+  const insideDiv = document.querySelectorAll('.profilemenu, .footer');
+  
+  insideDiv.forEach(function(inside) {
+    inside.addEventListener('click', function(e){
+        e.stopPropagation()
+      });
+  })
